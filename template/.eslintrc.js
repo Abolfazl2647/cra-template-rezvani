d6f39d74f52e0018f2b1ef6353c311789ad81702
Amir <returnamor@gmail.com>
2022-07-11 10:47:23 +0430

    chore: update template

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: [
-    'airbnb',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'eslint:recommended',
-    'prettier'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['react'],
-  rules: {
-    'react/forbid-prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/jsx-closing-bracket-location': 'off',
-    'react/jsx-no-duplicate-props': [2, { ignoreCase: false }]
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        paths: ['src']
-      }
+    extends: [
+        'airbnb',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'eslint:recommended',
+        'prettier'
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react'],
+    rules: {
+        'react/forbid-prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/jsx-closing-bracket-location': 'off',
+        'react/jsx-no-duplicate-props': [2, { ignoreCase: false }]
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                paths: ['src']
+            }
+        }
     }
-  }
 };
